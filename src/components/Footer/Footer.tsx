b5c1a2e0ec619ba65fd4b91c4d441a108bed1c9f
Yurii Keniu <kenu21@gmail.com>
2025-12-20 19:42:33 +0100

    feature: added small changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import styles from "./Footer.module.scss";
 
 export const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <nav className={styles.nav}>
-          <NavLink to="/" className={styles.link}>
-            Help Center
-          </NavLink>
-          <NavLink to="/" className={styles.link}>
-            Cookies & Interest-Based Ads
-          </NavLink>
-          <NavLink to="/" className={styles.link}>
-            Terms of Use
-          </NavLink>
-          <NavLink to="/" className={styles.link}>
-            Privacy Policy
-          </NavLink>
-        </nav>
         <p className={styles.copyright}>
           © 2025 CoffeeRadar and non-commercial team Mate.academy
         </p>
