42d8d882be38bbe8b7e386a5a9e15a6637c2a575
Yurii <yurij.kenu@gmail.com>
2025-12-22 16:07:06 +0100

    Revert "fix-filter: filter to local storage"
    This reverts commit e0743b11d0e0d34b6edc7df8a9d2e3683055696e.

--- src/utils/useFilterCount.ts
+++ src/utils/useFilterCount.ts
@@ -1,40 +1,7 @@
-import { useSearchParams } from 'react-router-dom';
-import { getFilters } from './filterService';
+import { useFilterContext } from '../utils/FilterContext';
 
 export const useFilterCount = () => {
-  const [searchParams] = useSearchParams();
+  const { filterCount } = useFilterContext();
 
-  const tagsFromUrl = searchParams.getAll('tags');
-  const priceFromUrl = searchParams.getAll('priceRating');
-  const ratingFromUrl = searchParams.getAll('rating');
-  const hasOpeningHoursParam = searchParams.has('openingHours');
-
-  const hasUrlFilters =
-    tagsFromUrl.length > 0 ||
-    priceFromUrl.length > 0 ||
-    ratingFromUrl.length > 0 ||
-    hasOpeningHoursParam;
-
-  if (hasUrlFilters) {
-    const tagsCount = tagsFromUrl.length;
-    const priceCount = priceFromUrl.length;
-    const ratingCount = ratingFromUrl.length;
-    const timeCount = hasOpeningHoursParam ? 1 : 0;
-
-    return tagsCount + priceCount + ratingCount + timeCount;
-  }
-
-  const storedFilters = getFilters();
-
-  if (!storedFilters) {
-    return 0;
-  }
-
-  const tagsCount = storedFilters.tags?.length ?? 0;
-  const priceCount = storedFilters.prices?.length ?? 0;
-  const ratingCount = storedFilters.rating?.length ?? 0;
-  const timeCount =
-    storedFilters.timeFrom && storedFilters.timeFrom !== '9:00 a.m.' ? 1 : 0;
-
-  return tagsCount + priceCount + ratingCount + timeCount;
+  return filterCount;
 };
